f1e14cc6480b7ed95bf407a79023951df2e54722
George Brindeiro <georgebrindeiro@lara.unb.br>
2015-08-19 12:30:22 -0300

    Renamed trike.py to the more appropriate control.py

--- scripts/control.py
+++ scripts/control.py
@@ -7,11 +7,14 @@ from std_msgs.msg import String
 
 def main():
     # init 'trike' node
-    rospy.init_node('trike', anonymous=True)
+    rospy.init_node('control', anonymous=True)
 
     # list subscribed topics
+    ## add imu angle
+    ## add imu speed
 
     # list published topics
+    ## add stim signal updates
 
     # define loop rate (in hz)
     rate = rospy.Rate(10)
@@ -19,10 +22,13 @@ def main():
     # node loop
     while not rospy.is_shutdown():
         # get some work done
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
+        ## get current angle position
+        ## get current angle speed and filter it
+        ## calculate control signal
+        ## saturate and scale control signal
 
         # publish work
+        ## send stim update
 
         # sleep until it's time to work again
         rate.sleep()
