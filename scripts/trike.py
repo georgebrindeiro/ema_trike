ce49c91424a21d3f447f322cfb70bbf582fdacd8
George Brindeiro <georgebrindeiro@lara.unb.br>
2015-08-19 11:38:43 -0300

    Reorganized code structure for trike.py so it's easier to understand

--- scripts/trike.py
+++ scripts/trike.py
@@ -1,14 +1,52 @@
 #!/usr/bin/env python
 
 import rospy
+
+# import ros msgs
 from std_msgs.msg import String
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+def main():
+    # init 'trike' node
+    rospy.init_node('trike', anonymous=True)
 
-    # fetch config file to initialize system variables
+    # list subscribed topics
+
+    # list published topics
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+
+    # fetch config params (don't forget to load trike.yaml to param server)
+    fetch_config()
+
+    # init realTimePlotter lists
+    init_realtimeplotter_lists()
+
+    # define loop rate (in hz)
+    rate = rospy.Rate(10)
+
+    # node loop
+    while not rospy.is_shutdown():
+        # get some work done
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+
+        # publish work
+        pub.publish(hello_str)
+
+        # sleep until it's time to work again
+        rate.sleep()
+
+def fetch_config():
+    global xRange
+    global filter_size
+    global gastrocnemius_max
+    global femoral_max
+    global speed_ref
+    global portIMU
+    global portStimulator
+    global addressPedal
+    global addressRemoteControl
+
+    # init variables fetched from param server
     xRange = rospy.get_param("xRange")
     filter_size = rospy.get_param("filter_size")
     gastrocnemius_max = rospy.get_param("gastrocnemius_max")
@@ -19,20 +57,32 @@ def talker():
     addressPedal = rospy.get_param("addressPedal")
     addressRemoteControl = rospy.get_param("addressRemoteControl")
 
-    # Initialize realTimePlotter lists
-    angle,angSpeed,angSpeedRefHistory,filtered_speed, \
-        signal_femoral,signal_gastrocnemius, \
-        controlSignal,errorHistory = [[0 for x in range(xRange)] for v in range(8)]
-    time_stamp = []
+def init_realtimeplotter_lists():
+    global angle
+    global angSpeed
+    global angSpeedRefHistory
+    global filtered_speed
+    global signal_femoral
+    global signal_gastrocnemius
+    global controlSignal
+    global errorHistory
+    global time_stamp
 
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+    # init realTimePlotter lists with zeros
+    angle,\
+        angSpeed,\
+        angSpeedRefHistory,\
+        filtered_speed,\
+        signal_femoral,\
+        signal_gastrocnemius,\
+        controlSignal,\
+        errorHistory = [[0 for x in range(xRange)] for v in range(8)]
+
+    # init time_stamp as an empty list
+    time_stamp = []
 
 if __name__ == '__main__':
     try:
-        talker()
+        main()
     except rospy.ROSInterruptException:
         pass
