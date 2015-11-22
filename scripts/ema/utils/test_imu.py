c39a9ee4bb3305ec8a6d6da9f0698991d11e72fd
George Brindeiro <georgebrindeiro@lara.unb.br>
2015-11-22 19:07:37 -0200

    Added wireless streaming test

--- scripts/ema/utils/test_imu.py
+++ scripts/ema/utils/test_imu.py
@@ -1,20 +1,61 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Created on Fri May 15 16:59:57 2015
 
-@author: Wall-e
-"""
-import serial
-import modules.imu
+import time
+import signal
+import sys
 
-port = 'COM5'
-address = 2
-serialPort = serial.Serial(port, timeout=1, writeTimeout=1, baudrate=115200)
-x = imu.IMU(serialPort,address)
-print 'Calibration: ' + x.calibrate()
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+        wl_device2.stopStreaming()
+        wl_device3.stopStreaming()
+        dng_device.close()
+        sys.exit(0)
 
-while True:
-    print 'Euler angles: ' + x.get_euler_angles();
+signal.signal(signal.SIGINT, signal_handler)
 
-serialPort.close()
+from ema.libs.yei import threespace_api as ts_api
+
+def run():
+    com_port = '/dev/ttyACM0'
+    dng_device = ts_api.TSDongle(com_port=com_port)
+
+    if dng_device is not None:
+
+        wl_device2 = ts_api.TSWLSensor(logical_id=2, dongle=dng_device)
+        wl_device3 = ts_api.TSWLSensor(logical_id=3, dongle=dng_device)
+
+        ## Get data from IMU #2 without streaming
+        print("==================================================")
+        print("Getting the filtered tared quaternion orientation for IMU #2.")
+        quat = wl_device2.getTaredOrientationAsQuaternion()
+        if quat is not None:
+            print(quat)
+        print("==================================================")
+        print("Getting the raw sensor data for IMU #2.")
+        data = wl_device2.getAllRawComponentSensorData()
+        if data is not None:
+            print("[%f, %f, %f] --Gyro\n[%f, %f, %f] --Accel\n[%f, %f, %f] --Comp"
+                                                                            % data)
+        print("==================================================")
+        print("Getting the LED color of the device for IMU #2.")
+        led = wl_device2.getLEDColor()
+        if led is not None:
+            print(led)
+        print("==================================================")
+
+        # Get data from IMU #3 with streaming
+        wl_device2.setStreamingTiming(interval = 1000, duration = 0, delay = 0)
+        wl_device2.setStreamingSlots(slot1 = 'getUntaredOrientationAsQuaternion')
+        wl_device2.startStreaming()
+
+        # Get data from IMU #3 with streaming
+        wl_device3.setStreamingTiming(interval = 1000, duration = 0, delay = 0)
+        wl_device3.setStreamingSlots(slot1 = 'getUntaredOrientationAsQuaternion',)
+        wl_device3.startStreaming()
+
+        while(True):
+            ts2, data2 = wl_device2.getLatestStreamData(10.0)
+            ts3, data3 = wl_device3.getLatestStreamData(10.0)
+            print "[imu #2] ts = {0}, data = {1}".format(ts2,data2)
+            print "[imu #3] ts = {0}, data = {1}".format(ts3,data3)
---
+++ scripts/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ema.utils import test_imu
+
+test_imu.run()
