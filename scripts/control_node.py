5955f7dda120d9d8de1b773fb7f1c770429564aa
George Brindeiro <georgebrindeiro@lara.unb.br>
2015-12-16 23:25:42 -0200

    Basic controller implemented, not tested yet

--- scripts/control_node.py
+++ scripts/control_node.py
@@ -1,38 +1,85 @@
 #!/usr/bin/env python
 
 import rospy
+import ema.modules.control as control
 
 # import ros msgs
-from std_msgs.msg import String
+from sensor_msgs.msg import Imu
+from ema_common_msgs.msg import Stimulator
+
+# import utilities
+from math import pi
+from tf import transformations
+
+# global variables
+angle = [0,0]
+speed = [0,0]
+speed_ref = 300
+speed_err = [0,0]
+time = [0,0]
+pw_left = [0,0]
+pw_right = [0,0]
+
+
+def callback(data):
+    # get timestamp
+    time.append(data.header.stamp)
+
+    # get angle position
+    qx,qy,qz,qw = data.orientation.x,data.orientation.y,data.orientation.z,data.orientation.w
+    euler = transformations.euler_from_quaternion([qx, qy, qz, qw], axes='syxz')
+    if euler[0] >= 0:
+        angle.append(euler[0]*(180/pi))
+    else:
+        angle.append(360+euler[0]*(180/pi))
+        
+    # get angular speed
+    speed.append(data.angular_velocity.y*(180/pi))
+
+    # get error
+    speed_err.append(speed_ref - speed[-1])
+
+    # print latest
+    #print time[-1], angle[-1], speed[-1], speed_err[-1]
 
 def main():
-    # init 'trike' node
-    rospy.init_node('control', anonymous=True)
-
+    # init control node
+    controller = control.Control(rospy.init_node('control', anonymous=False))
+    
+    # get control config
+    config_dict = rospy.get_param('/ema_trike/control')
+    
     # list subscribed topics
-    ## add imu angle
-    ## add imu speed
-
+    sub = rospy.Subscriber('imu/pedal', Imu, callback = callback)
+    
     # list published topics
-    ## add stim signal updates
-
+    pub = rospy.Publisher('stimulator/ccl_update', Stimulator, queue_size=10)
+    
     # define loop rate (in hz)
     rate = rospy.Rate(10)
-
+    
+    # build basic stimulator message
+    stimMsg = Stimulator()
+    stimMsg.channel = [1, 2]
+    stimMsg.mode = ['single', 'single']
+    stimMsg.pulse_current = [6, 6]
+    
     # node loop
     while not rospy.is_shutdown():
-        # get some work done
-        ## get current angle position
-        ## get current angle speed and filter it
-        ## calculate control signal
-        ## saturate and scale control signal
-
-        # publish work
-        ## send stim update
-
-        # sleep until it's time to work again
+        # calculate control signal
+        pwl, pwr = controller.calculate(angle[-1], speed[-1], speed_ref, speed_err)
+        
+        # send stimulator update
+        stimMsg.pulse_width = [pw_left[-1], pw_right[-1]]
+        pub.publish(stimMsg)
+        
+        # store control signal for plotting
+        pw_left.append(pwl)
+        pw_right.append(pwr)
+        
+        # wait for next control loop
         rate.sleep()
-
+        
 if __name__ == '__main__':
     try:
         main()
--- scripts/ema/modules/control.py
+++ scripts/ema/modules/control.py
@@ -1,32 +1,68 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May 23 17:55:13 2015
+#!/usr/bin/env python
+theta = {
+    'left': {'min': 300, 'max': 40},
+    'right': {'min': 120, 'max': 220},
+    'shift': 35
+}
 
-@author: Wall-e
-"""
-
-def control(error):
-    signal = 0
-    try:    
-        p = 1/float(1000)
-        i = 1/float(100000)
+class Control:
+    def __init__(self, config_dict):
+        self.config_dict = config_dict
+        
+    def fx(self, id, angle, speed, speed_ref):
+        dth = (speed/speed_ref)*theta['shift']
+        
+        theta_min = theta[id]['min'] - dth
+        theta_max = theta[id]['max'] - dth
+        
+        # check if angle in range (theta_min, theta_max)
+        if theta_min <= angle and angle <= theta_max:
+            return 1
+        elif theta[id]['min'] > theta[id]['max']:
+            if angle <= theta_min and angle <= theta_max:
+                if theta_min <= angle + 360 and angle <= theta_max:
+                    return 1
+            elif angle >= theta_min and angle >= theta_max:
+                if theta_min <= angle and angle <= theta_max + 360:
+                    return 1
+                    
+        # return 0 otherwise
+        return 0
+    
+    def g(self, error):
+        Kp = 0.05
+        Ki = 0.005
         
         # If there is a change of signal, reset
-#        if ((error[-2] >= 0) and (error[-1] < 0)) or ((error[-2] < 0) and (error[-1] >= 0)):
-#            errorTemp = [0 for x in range(len(error))]
-#            errorTemp[-1] = error[-1]
-#            error = errorTemp
+        if ((error[-2] >= 0) and (error[-1] < 0)) or ((error[-2] < 0) and (error[-1] >= 0)):
+            errorTemp = [0 for x in range(len(error))]
+            errorTemp[-1] = error[-1]
+            error = errorTemp
         
-        signal = 0.5 + p*error[-1]+i*sum(error)
+        signal = 0.5 + Kp*error[-1]+Ki*sum(error)
         
-#         saturation
+        # saturation
         if signal > 1:
             signal = 1
             error[-1] = 0
         elif signal < 0:
             signal = 0
             error[-1] = 0
+        
+        return signal
             
-    except ValueError:
-        return "Control error"
-    return signal
+    def calculate(self, angle, speed, speed_ref, speed_err):
+        pw_max = 500
+        
+        fx_left = self.fx('left', angle, speed, speed_ref)
+        fx_right = self.fx('right', angle, speed, speed_ref)
+        
+        g = self.g(speed_err)
+        
+        pw_left = fx_left*g*pw_max
+        pw_right = fx_right*g*pw_max
+        
+        # print g, pw_left, pw_right
+        # print [round(x) for x in speed_err[-10:]]
+        
+        return pw_left, pw_right
